a421d192aee507463f27cbaf4afac885373b10ef
Phospholite <168874131+Haruno123@users.noreply.github.com>
2024-07-03 18:58:32 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import random
+import datetime
 
 app = Flask(__name__)
 
@@ -14,21 +15,37 @@ facts_list = [
     "Las redes sociales tienen aspectos positivos y negativos, y debemos ser conscientes de ambos cuando utilicemos estas plataformas."
 ]
 
+def is_christmas():
+    today = datetime.date.today()
+    return today.month == 12 and today.day == 25
+
 @app.route('/')
 def home():
     return '''
         <h1>Bienvenido</h1>
-        <a href="/random_fact">¡Ver un dato aleatorio!</a>
-        <iframe src="https://www.meteoblue.com/en/weather/widget/daily/ip_romania_675514?geoloc=fixed&days=4&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&precipunit=MILLIMETER&coloured=coloured&pictoicon=0&pictoicon=1&maxtemperature=0&maxtemperature=1&mintemperature=0&mintemperature=1&windspeed=0&windspeed=1&windgust=0&winddirection=0&winddirection=1&uv=0&humidity=0&precipitation=0&precipitation=1&precipitationprobability=0&precipitationprobability=1&spot=0&spot=1&pressure=0&layout=light"  frameborder="0" scrolling="NO" allowtransparency="true" sandbox="allow-same-origin allow-scripts allow-popups allow-popups-to-escape-sandbox" style="width: 216px; height: 432px"></iframe><div><!-- DO NOT REMOVE THIS LINK --><a href="https://www.meteoblue.com/en/weather/week/ip_romania_675514?utm_source=daily_widget&utm_medium=linkus&utm_content=daily&utm_campaign=Weather%2BWidget" target="_blank" rel="noopener">meteoblue</a></div>
+        <a href="/random_fact">¡Ver un dato aleatorio!</a><br>
+        <a href="/is_christmas">¿Es Navidad?</a>
+        <iframe src="https://www.meteoblue.com/en/weather/widget/daily/ip_romania_675514?geoloc=fixed&days=4&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&precipunit=MILLIMETER&coloured=coloured&pictoicon=0&pictoicon=1&maxtemperature=0&maxtemperature=1&mintemperature=0&mintemperature=1&windspeed=0&windspeed=1&windgust=0&winddirection=0&winddirection=1&uv=0&humidity=0&precipitation=0&precipitation=1&precipitationprobability=0&precipitationprobability=1&spot=0&spot=1&pressure=0&layout=light" frameborder="0" scrolling="NO" allowtransparency="true" sandbox="allow-same-origin allow-scripts allow-popups allow-popups-to-escape-sandbox" style="width: 216px; height: 432px"></iframe>
+        <div><!-- DO NOT REMOVE THIS LINK --><a href="https://www.meteoblue.com/en/weather/week/ip_romania_675514?utm_source=daily_widget&utm_medium=linkus&utm_content=daily&utm_campaign=Weather%2BWidget" target="_blank" rel="noopener">meteoblue</a></div>
     '''
 
 @app.route('/random_fact')
 def random_fact():
-    # Selección de un hecho aleatorio y creación de una página con el hecho, un enlace para volver a la página principal y el iframe
     fact = random.choice(facts_list)
-    return (f'''
+    return f'''
+        <h1>Dato Aleatorio</h1>
         <p>{fact}</p>
         <a href="/">Volver</a>
-    ''')
+    '''
 
-app.run(debug=True)
+@app.route('/is_christmas')
+def check_christmas():
+    christmas_message = "¡Es Navidad!" if is_christmas() else "No es Navidad."
+    return f'''
+        <h1>¿Es Navidad?</h1>
+        <p>{christmas_message}</p>
+        <a href="/">Volver</a>
+    '''
+
+if __name__ == "__main__":
+    app.run(debug=True)
